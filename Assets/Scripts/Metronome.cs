c59312c745b8d8e142d2678c302fdc07857ac240
Henrique Chaltein <hxs1151@rit.edu>
2015-10-06 14:14:40 -0400

    Metronome (Audio) functioning

--- Assets/Scripts/Metronome.cs
+++ Assets/Scripts/Metronome.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Metronome : MonoBehaviour {
+public class Metronome : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+    public float BPS;
+    public bool isPlaying;
+
+    AudioSource sndPlyr;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        isPlaying = false;
+
+        sndPlyr = GetComponent<AudioSource>();
+
+    }
+
+    // Use this for initialization
+    void Update ()
+    {
+        // Check for sound player
+        if (sndPlyr == null)
+        {
+            Debug.Log("No Audio source Component!");
+        }
+
+        // Check for spaceBar
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Check if it was playing
+            if(isPlaying)
+            {
+                // Stop playing
+                isPlaying = false;
+            }
+            else 
+            {
+                // Start playing
+                isPlaying = true;
+                StartCoroutine(PlayMetroBeat(BPS));
+            }
+        }
 	}
+
+    IEnumerator PlayMetroBeat(float BPS)
+    {
+        //  converts BPs into the wait time(seconds per beat)
+        float waitTime = (1 / BPS);
+        while(isPlaying)
+        {
+            yield return new WaitForSeconds(waitTime);
+            sndPlyr.PlayOneShot(sndPlyr.clip);
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
+
