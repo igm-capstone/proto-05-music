619b37a244edefd2b6d1c7c297554d13cf4c43cc
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-10-12 19:45:05 -0400

    Debug lines for positioning.

--- Assets/Scripts/CollectibleBehavior.cs
+++ Assets/Scripts/CollectibleBehavior.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 [RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Collider2D))]
-
+[ExecuteInEditMode]
 public class CollectibleBehavior : MonoBehaviour
 {
     public Collider2D   m_Collider;
     public AudioSource  m_audioSource;
     public bool         m_AnimatesPlayer;
     private ComposerBehaviour jukebox;
+    public float        m_playerSpeed;
+    private Vector3     m_DebugRay;
 
     public float lengthInBeats  = 1.0f;
     private bool m_isCollected   = false;
@@ -18,7 +21,17 @@ public class CollectibleBehavior : MonoBehaviour
         m_audioSource   = GetComponent<AudioSource>();
         m_Collider      = GetComponent<Collider2D>();
         jukebox         = FindObjectOfType<ComposerBehaviour>();
-	}
+        m_playerSpeed   = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().RunSpeed;
+        m_DebugRay      = (transform.right * m_playerSpeed * m_audioSource.clip.length);
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 p = transform.position + m_DebugRay;
+        Debug.DrawLine(transform.position, p);
+
+        Handles.Label(p, "Data: x:"+ p.x + "y:" + p.y);
+    }
 
     public void PlayAudio(Collider2D player)
     {
