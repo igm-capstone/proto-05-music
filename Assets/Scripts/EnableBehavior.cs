8b8277d0409cb9c6080d9bc316e93467b706e8c4
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-10-13 14:53:38 -0400

    Section 3

---
+++ Assets/Scripts/EnableBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+public class EnableBehavior : MonoBehaviour {
+
+    public GameObject ObjectToEnable;
+
+	// Use this for initialization
+	void Start () {
+        ObjectToEnable.SetActive(false);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<CharacterController2D>().onTriggerExitEvent += TriggerExit;
+    }
+	
+	// Update is called once per frame
+	public void EnableObject()
+    {
+        ObjectToEnable.SetActive(true);
+    }
+
+    void TriggerExit(Collider2D other)
+    {
+        if (other.gameObject.transform == transform)
+        {
+            EnableObject();
+        }
+    }
+}
